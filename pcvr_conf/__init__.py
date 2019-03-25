77e381c8fe69cd68df2c5ac969000fdc9d6e1466
Chenpengpeng0715 <46666848+Chenpengpeng0715@users.noreply.github.com>
2019-03-25 10:51:26 +0800

    Add files via upload

---
+++ pcvr_conf/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# author: cwx520375 
+# time: 2019/3/11
+
+ 
---
+++ pcvr_conf/config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# author: cwx520375 
+# time: 2019/3/11
+
+
+import os
+
+
+curr_path = r"C:\Users\cwx520375\Desktop\105"
+common_path = r""
+# app_path = os.path.join(curr_path, "")
+client_path = os.path.join(curr_path, "HwPCVRAssistant_9.0.0.105.exe")
+client_pic_path = os.path.join(common_path, "")
+
